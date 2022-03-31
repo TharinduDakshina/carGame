d71c0125fd0e17f59e557921aa27ec6c71122e46
TharinduDakshina <tharindudakshina527@gmail.com>
2022-03-31 11:39:43 +0530

    random block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,21 @@
 function moveLeft(){
     let left= parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     left -=100;
-    character.style.left=left+"px";
+    if(left>-100){
+        character.style.left=left+"px";
+    }
 }
 
 function moveRight(){
     let left= parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     left +=100;
-    character.style.left=left+"px";
+    if(left<500){
+        character.style.left=left+"px";
+    }
+    
 }
 
-// $("#character").on("keydown",function(event){
-//     if(event.key=="ArrowLeft"){
-//         moveLeft();
-//     }
-
-//     if(event.key=="ArrowRight"){
-//         moveRight();
-//     }
-// });
-
-document.addEventListener("keydown",function(event){
+$(this).on("keydown",function(event){
     if(event.key=="ArrowLeft"){
         moveLeft();
     }
@@ -28,4 +23,16 @@ document.addEventListener("keydown",function(event){
     if(event.key=="ArrowRight"){
         moveRight();
     }
-})
+});
+
+$("#block").on("animationiteration",function(){
+    var block=$("#block");
+    var randomBlock=Math.floor(Math.random()*5);
+    var left =randomBlock*100;
+    console.log(left);
+    var l=left+"px";
+   $("#block").css("left",l);
+   console.log(l);
+    //block.style.left=left+"px";
+});
+
