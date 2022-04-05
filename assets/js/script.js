5bf006dbf2ad0d56d6c9458d4bfe7cb27ce319e6
TharinduDakshina <tharindudakshina527@gmail.com>
2022-04-05 20:17:17 +0530

    replace logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ function moveRight(){
     if(left<500){
         character.style.left=left+"px";
     }
-    
+
 }
 
 $(this).on("keydown",function(event){
@@ -31,28 +31,28 @@ $("#block2").on("animationiteration",function(){
     var randomBlock=Math.floor(Math.random()*5);
     var left =randomBlock*100;
     var l=left+"px";
-   $("#block2").css("left",l);
-   count++;
+    $("#block2").css("left",l);
+    count++;
 });
 
 $("#block").on("animationiteration",function(){
     var randomBlock=Math.floor(Math.random()*5);
     var left =randomBlock*100;
     var l=left+"px";
-   $("#block").css("left",l);
-   count++;
+    $("#block").css("left",l);
+    count++;
 });
 
 setInterval(function(){
     var characterLeft=parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    console.log("characterLeft : "+characterLeft);
+
 
     var blockLeft=parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 
-    console.log("blockLeft : "+blockLeft);
+
     var blockTop=parseInt(window.getComputedStyle(block).getPropertyValue("top"));
 
-   /* console.log("blockTop : "+blockTop);*/
+
     var block2Left=parseInt(window.getComputedStyle(block2).getPropertyValue("left"));
 
 
@@ -60,12 +60,12 @@ setInterval(function(){
 
     if( characterLeft==blockLeft && blockTop<700 && blockTop>600){
         console.log("true ");
-            alert("Game Over . Score ---> "+count);
-            $("#block").css("animation","none");
-            $("#block2").css("animation","none");
+        alert("Game Over . Score ---> "+count);
+        $("#block").css("animation","none");
+        $("#block2").css("animation","none");
     }else if(characterLeft==block2Left && block2Top<700 && block2Top>500){
         alert("Game Over . Score---> "+count);
         $("#block2").css("animation","none");
         $("#block").css("animation","none");
     }
-},10000);
+},10);
